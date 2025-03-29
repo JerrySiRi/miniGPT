d91e205a566bd67e53b0d6db487ad10e6d839afe
JerrySiRi <15210723175@163.com>
2025-03-29 18:24:57 +0800

    add dafny example

--- Dafny/Dafny_feedback.py
+++ Dafny/Dafny_feedback.py
@@ -9,3 +9,21 @@ def run_dafny():
     return result.stdout  # 返回编译器输出
 
 run_dafny()
+
+
+def cal_AUCPR_AP(labels, scores):
+    auc_pr = ap = 0
+    tp_and_fn = labels.count(1)
+    precision = [1, 0]
+    recall = [0, 0]
+    pairs = sorted(zip(scores, labels), reverse=True)
+    for i in range(len(pairs)):
+        precision[1] = (precision[0]*i + int(pairs[i][1] == 1))/(i+1)
+        recall[1] = recall[0] + int(pairs[i][1] == 1)/tp_and_fn
+        auc_pr += (recall[1]-recall[0]) * (precision[1]+precision[0]) / 2
+        ap += (recall[1]-recall[0]) * precision[1]
+        precision[0], recall[0] = precision[1], recall[1]
+    return auc_pr, ap
+labels = [1,2,1,1,2,1,2,2,1,2]
+scores = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+print(cal_AUCPR_AP(labels, scores))
